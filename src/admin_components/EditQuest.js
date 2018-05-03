ee3ac3589dff37d4f989564886a241821958861a
Sergey Shakur <sergey.shakur@gmail.com>
2018-05-03 10:56:53 +0300

    implemented possibility to add questions

--- src/admin_components/EditQuest.js
+++ src/admin_components/EditQuest.js
@@ -5,9 +5,11 @@ import { fetchQuest, updateQuest } from '../api/api.js'
 import EditableEntry from './EditableEntry.js'
 import Accordion from './Accordion.js'
 
+
 export default class extends PureComponent {
     constructor(props) {
         super(props)
+        this.QUESTIONS_ARRAY_NAME = 'questions'
         this.state = {
             quest: undefined,
             unsavedQuest: {},
@@ -15,6 +17,8 @@ export default class extends PureComponent {
             shouldRedirectBack: false
         }
     }
+
+    generateRandomAlphanumeric = () => Math.random().toString(36).substr(2, 9);
 
     componentDidMount() {
         fetchQuest(this.props.match.params.questId)
@@ -30,18 +34,32 @@ export default class extends PureComponent {
         this.setState({ unsavedQuest: { ...this.state.unsavedQuest, [propName]: propValue } })
     }
 
-    setNewValueInArray(arrayName, index, propName, propValue) {
-        let arr = [...this.state.unsavedQuest[arrayName]];
+    addItemToQuestionsArray() {
+        let arr = [...this.state.unsavedQuest[this.QUESTIONS_ARRAY_NAME]];
+        const question = {
+            text: "",
+            answer: "",
+            hint: "",
+            __id: this.generateRandomAlphanumeric()
+        };
+        arr.push(question);
+        this.setNewValue(this.QUESTIONS_ARRAY_NAME, arr);
+    }
+
+    setNewValueForQuestionsItem(__id, propName, propValue) {
+        let arr = [...this.state.unsavedQuest[this.QUESTIONS_ARRAY_NAME]];
+        let index = arr.findIndex(e => e.__id == __id);
         let item = { ...arr[index] };
         item[propName] = propValue;
         arr[index] = item;
-        this.setNewValue(arrayName, arr);
+        this.setNewValue(this.QUESTIONS_ARRAY_NAME, arr);
     }
 
-    removeValueFromArray(arrayName, index) {
-        let arr = [...this.state.unsavedQuest[arrayName]];
+    removeItemFromQuestionsArray(__id) {
+        let arr = [...this.state.unsavedQuest[this.QUESTIONS_ARRAY_NAME]];
+        const index = arr.findIndex(e => e.__id == __id);
         arr.splice(index, 1);
-        this.setNewValue(arrayName, arr);
+        this.setNewValue(this.QUESTIONS_ARRAY_NAME, arr);
     }
 
     submitChanges() {
@@ -72,34 +90,40 @@ export default class extends PureComponent {
                         <EditableEntry
                             title='Quest name:'
                             content={this.state.quest.name}
+                            unsavedContent={this.state.unsavedQuest.name}
                             propagateContent={content => this.setNewValue('name', content)}
                             isEditMode={this.state.isEditMode} />
 
                         <EditableEntry
                             title='Quest id:'
                             content={this.state.quest.id}
+                            unsavedContent={this.state.unsavedQuest.id}
                             propagateContent={content => this.setNewValue('id', content)}
                             isEditMode={this.state.isEditMode} />
 
                         <EditableEntry
                             title='Quest intro:'
                             content={this.state.quest.intro}
+                            unsavedContent={this.state.unsavedQuest.intro}
                             propagateContent={content => this.setNewValue('intro', content)}
                             isEditMode={this.state.isEditMode} />
 
                         <EditableEntry
                             title='Final words:'
                             content={this.state.quest.finalWords}
+                            unsavedContent={this.state.unsavedQuest.finalWords}
                             propagateContent={content => this.setNewValue('finalWords', content)}
                             isEditMode={this.state.isEditMode} />
 
                         <div className='accordion-holder'>
                             <Accordion
                                 title='Questions:'
-                                propagateValue={(index, key, value) => this.setNewValueInArray('questions', index, key, value)}
-                                removeItem={index => this.removeValueFromArray('questions', index)}
                                 isEditMode={this.state.isEditMode}
-                                data={this.state.quest.questions ? this.state.quest.questions : []}
+                                content={this.state.quest.questions ? this.state.quest.questions : []}
+                                unsavedContent={this.state.unsavedQuest.questions ? this.state.unsavedQuest.questions : []}
+                                propagateArrayValue={(__id, key, value) => this.setNewValueForQuestionsItem(__id, key, value)}
+                                removeItem={__id => this.removeItemFromQuestionsArray(__id)}
+                                addItem={() => this.addItemToQuestionsArray()}
                             />
                         </div>
 
