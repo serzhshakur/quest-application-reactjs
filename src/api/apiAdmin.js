8873c005b72ebc47e7649f4560c9cf8dcb805be1
Sergey Shakur <sergey.shakur@gmail.com>
2018-06-04 22:53:05 +0300

    path to backend admin routes changed

--- src/api/apiAdmin.js
+++ src/api/apiAdmin.js
@@ -19,19 +19,19 @@ export function loginToAdmin(username, password) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
     };
-    return fetch(`${baseUrl}/admin/login`, options)
+    return fetch(`${baseUrl}/my-admin/login`, options)
         .then(response => handleResponseForAuthorizedCall(response))
         .then(responseJson => sessionStorage.token = responseJson.token)
 }
 
 export function fetchQuests() {
-    return fetch(`${baseUrl}/admin/quests`,
+    return fetch(`${baseUrl}/my-admin/quests`,
         { headers: { 'x-access-token': sessionStorage.token } })
         .then(response => handleResponseForAuthorizedCall(response))
 }
 
 export function fetchQuest(questId) {
-    return fetch(`${baseUrl}/admin/quests/${questId}`,
+    return fetch(`${baseUrl}/my-admin/quests/${questId}`,
         { headers: { 'x-access-token': sessionStorage.token } })
         .then(response => handleResponseForAuthorizedCall(response))
 }
@@ -42,7 +42,7 @@ export function postQuest(questDetails) {
         headers: { 'Content-Type': 'application/json', 'x-access-token': sessionStorage.token },
         body: JSON.stringify(questDetails)
     }
-    return fetch(`${baseUrl}/admin/quests`, options)
+    return fetch(`${baseUrl}/my-admin/quests`, options)
         .then(response => handleResponseForAuthorizedCall(response))
 }
 
@@ -52,6 +52,6 @@ export function updateQuest(questId, questDetails) {
         headers: { 'Content-Type': 'application/json', 'x-access-token': sessionStorage.token },
         body: JSON.stringify(questDetails)
     }
-    return fetch(`${baseUrl}/admin/quests/${questId}`, options)
+    return fetch(`${baseUrl}/my-admin/quests/${questId}`, options)
         .then(response => handleResponseForAuthorizedCall(response))
 }
